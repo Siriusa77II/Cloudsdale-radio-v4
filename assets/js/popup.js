fd114a13094e4a6a1c5fdcd2249b0208924d9ecb
Quentin Silva <Sirius@SiriusHD.net>
2017-11-25 13:12:57 +0100

    UPDATE everythings for 4.0 Winter edition

---
+++ assets/js/popup.js
@@ -0,0 +1,26 @@
+// popup.js
+// Created by Sirius
+//for Cloudsdale Radio & EquestriaFM
+// v 1.2
+jQuery(document).ready(function($) {
+
+	$('.popup').click(function() {
+	
+		var w = $(this).data('width');
+		var h = $(this).data('height');
+		var s = $(this).data('scrollbars');
+
+		var left = (screen.width/2) - (w/2);
+		var top = (screen.height/2) - (h/2);
+
+		var NWin = window.open($(this).prop('href'),'','scrollbars=' + s + ',resizable=yes,width=' + w + ',height=' + h + ',top=' + top + ',left=' + left);
+
+		if (window.focus)
+		
+			{
+				NWin.focus();
+			}
+			
+		return false;
+	});
+});
